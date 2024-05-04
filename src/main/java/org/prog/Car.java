f652085a9628502d1ff1492c790e4a4aa0073cc8
Ihor <sakun29@gmail.com>
2024-05-05 00:40:42 +0300

    Homework 2

--- src/main/java/org/prog/Car.java
+++ src/main/java/org/prog/Car.java
@@ -3,28 +3,40 @@ package org.prog;
 public class Car implements IMovable {
 
     public String color;
-
+    public String plateNumber;
     public void goTo() {
         System.out.println(color + " car goes somewhere");
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Car){
-            boolean match = this.color.equals(((Car) obj).color);
-            return match;
+    public boolean equals(Object obj)
+    {
+        if (obj instanceof Car)
+        {
+            Car car = (Car) obj;
+            if (this.color == null || this.plateNumber == null || car.color == null || car.plateNumber == null)
+            {
+                throw new RuntimeException("Color and Plate Number cannot be null");
+            }
+            return this.color.equals(car.color) && this.plateNumber.equals(car.plateNumber);
         }
         return false;
     }
 
+
     @Override
-    public int hashCode(){
-        return this.color.hashCode();
+    public int hashCode()
+    {
+        if (this.color == null || this.plateNumber == null) {
+        throw new RuntimeException("Color and Plate Number cannot be null");
+    }
+        return this.color.hashCode() + this.plateNumber.hashCode();
     }
 
     @Override
-    public String toString(){
-        return "This car's color is " + color;
+    public String toString()
+    {
+        return "This car's color is " + color + " and plate number is " + plateNumber;
     }
 }
 
--- src/main/java/org/prog/exceptions/HomeWork.java
+++ src/main/java/org/prog/exceptions/HomeWork.java
@@ -1,5 +1,5 @@
 package org.prog.exceptions;
-
+import org.prog.Car;
 public class HomeWork {
 
     /**
@@ -8,10 +8,24 @@ public class HomeWork {
      * Make Car's hashCode method work for color AND plateNumber
      *
      * * - if car has null color or null plate number - throw RuntimeException in equals and HashCode method
-     * @param args
+     *@param args
      */
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.color = "Red";
+        myCar.plateNumber = "123ABC";
+        myCar.goTo();
+        System.out.println(myCar.toString());
+        Car anotherCar = new Car();
+        anotherCar.color = "Red";
+        anotherCar.plateNumber = "123ABC";
+        if (myCar.equals(anotherCar)) {
+            System.out.println("Cars are equal");
+        } else {
+            System.out.println("Cars are not equal");
+        }
     }
+
 }
