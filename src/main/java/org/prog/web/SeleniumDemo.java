41e10583ba81ebe2bfb09cb620a6a7e67a489405
Ihor <sakun29@gmail.com>
2024-05-25 16:58:13 +0300

    Homework 4

--- src/main/java/org/prog/web/SeleniumDemo.java
+++ src/main/java/org/prog/web/SeleniumDemo.java
@@ -5,42 +5,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SeleniumDemo {
 
     /*
      * 1. Open browser +
      * 2. Load allo.ua +
-     * 3. Click Акції +
-     * 4. Click Present +
-     * 5. Click first present +
-     * 6. Print present's name
+     * 3. Click profile button +
+     * 4. Click login and password +
+     * 5. Enter incorrect login and password +
+     * 6. Click login button +
+     * 7. Print error message
      */
 
     public static void main(String[] args) {
-        //start browser
+        // Start browser
         WebDriver driver = new ChromeDriver();
         try {
-            //full screen
+            // Maximize window
             driver.manage().window().maximize();
-            //load allo ua
+            // Set implicit wait
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            // Load allo.ua
             driver.get("https://allo.ua/");
-            //select discounts
-            WebElement discountsBtn = driver.findElement(By.partialLinkText("Акції"));
-            discountsBtn.click();
-            // click presents
-            WebElement presentBtn = driver.findElement(By.partialLinkText("Подарунки"));
-            presentBtn.click();
-            //find promo items parent element
-            WebElement promoParent = driver.findElement(By.className("promo-list__items"));
-            //find and click 1st promo item
-            List<WebElement> promoList = promoParent.findElements(By.tagName("a"));
-            WebElement firstPromo = promoList.get(0);
-            firstPromo.click();
-            // print promo title
-            WebElement promoTitle = driver.findElement(By.className("promo-info__title"));
-            System.out.println(promoTitle.getText());
+
+            WebElement profileButton = driver.findElement(By.xpath("//button[@aria-label='Профіль']"));
+            profileButton.click();
+            // Click login and password
+            WebElement loginPasswordBtn = driver.findElement(By.xpath("//span[contains(text(), 'Логін та пароль')]"));
+            loginPasswordBtn.click();
+
+            WebElement loginField = driver.findElement(By.name("phoneEmail"));
+            loginField.clear();
+            loginField.sendKeys("ффф");
+            WebElement passwordField = driver.findElement(By.name("password"));
+            passwordField.clear();
+            passwordField.sendKeys("");
+
+            WebElement loginBtn = driver.findElement(By.xpath("//span[contains(text(), 'Увійти')]"));
+            loginBtn.click();
+
+            WebElement loginErrorMessage = driver.findElement(By.xpath("//span[@class='a-input__message base-message is-error'][contains(text(),'некоректний email')]"));
+            System.out.println("Логін: " + loginErrorMessage.getText());
+            WebElement passwordErrorMessage = driver.findElement(By.xpath("//span[contains(@class,'a-input__message')][contains(text(),\"Це поле є обов'язковим для заповнення\")]"));
+            System.out.println("Пароль: " + passwordErrorMessage.getText());
         } finally {
             driver.quit();
         }
